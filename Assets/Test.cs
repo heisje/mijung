7e64529daa4191c3cd682ea53807660b88cd0734
heisje <heeje1996@gmail.com>
2024-07-23 00:02:15 +0900

    duck

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // 개십덕 김민수
+        // 개십덕 김민수 ++ 
     }
 
     // Update is called once per frame
